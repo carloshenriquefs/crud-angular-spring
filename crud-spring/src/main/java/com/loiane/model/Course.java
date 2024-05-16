67eac2cae215f85d4c8c525ea67ca359f73b4a1e
Carlos Henrique <ichfs9108@gmail.com>
2024-05-16 16:01:31 -0300

    Listar Cursos (API HTTP GET)

---
+++ crud-spring/src/main/java/com/loiane/model/Course.java
@@ -0,0 +1,20 @@
+package com.loiane.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false)
+    private String category;
+}
