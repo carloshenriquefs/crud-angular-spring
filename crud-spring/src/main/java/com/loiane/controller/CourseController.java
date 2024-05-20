110c5f671ba3fe625f7071a2f3e41e6bf2384392
Carlos Henrique <ichfs9108@gmail.com>
2024-05-20 16:26:10 -0300

    Formulario: API Spring: Criar Curso (HTTP POST)

--- crud-spring/src/main/java/com/loiane/controller/CourseController.java
+++ crud-spring/src/main/java/com/loiane/controller/CourseController.java
@@ -3,9 +3,8 @@ package com.loiane.controller;
 import com.loiane.model.Course;
 import com.loiane.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,11 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
