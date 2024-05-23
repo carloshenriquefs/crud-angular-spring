f44239d7e27612ea29128477b7c1ee20afd13e9e
Carlos Henrique <ichfs9108@gmail.com>
2024-05-23 19:55:43 -0300

    API Spring: Buscar por ID

--- crud-spring/src/main/java/com/loiane/controller/CourseController.java
+++ crud-spring/src/main/java/com/loiane/controller/CourseController.java
@@ -4,7 +4,9 @@ import com.loiane.model.Course;
 import com.loiane.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,13 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
