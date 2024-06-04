59dddcf009bfc71878aed97fc2ba8af768267cb8
Carlos Henrique <ichfs9108@gmail.com>
2024-06-04 03:28:12 -0300

    feat: Curso Aulas: OneToMany

--- crud-spring/src/main/java/com/loiane/model/Course.java
+++ crud-spring/src/main/java/com/loiane/model/Course.java
@@ -5,12 +5,14 @@ import com.loiane.enums.Category;
 import com.loiane.enums.Status;
 import com.loiane.enums.converters.CategoryConverter;
 import com.loiane.enums.converters.StatusConverter;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -18,6 +20,9 @@ import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -46,4 +51,8 @@ public class Course {
     @Column(length = 10, nullable = false)
     @Convert(converter = StatusConverter.class)
     private Status status = Status.ACTIVE;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
+    private List<Lesson> lessons = new ArrayList<>();
+
 }
